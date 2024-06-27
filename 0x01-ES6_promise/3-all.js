981d11cfa590ab1558420558207fc86449b7d034
Gigi2303 <ajibola.grace201@gmail.com>
2024-06-26 20:51:37 -0700

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export handleProfileSignup;
