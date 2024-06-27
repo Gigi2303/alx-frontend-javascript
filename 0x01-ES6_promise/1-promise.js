ce1539e9c4166c1f05f93710a0bb41e407f25f9d
Gigi2303 <ajibola.grace201@gmail.com>
2024-06-26 20:03:04 -0700

    2

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    }, 2000);
+  })
+    .catch(error => {
+      console.error('Error in getFullResponseFromAPI:', error.message);
+      throw error;
+    });
+  }
+
+export default getFullResponseFromAPI;
