ce503cf09f586c6e866bd4dcc2f9b452276f44d2
Gigi2303 <ajibola.grace201@gmail.com>
2024-07-02 20:25:10 -0700

    done

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function hasValuesFromArray(set, arr) {
+  for (const element of arr) {
+    if (!set.has(element)) {
+      return false;
+    }
+  }
+  return true;
+}
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString = '') {
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+
+  let returnString = '';
+  const startStringLength = startString.length;
+  set.forEach((string) => {
+    if (string !== undefined && string.startsWith(startString)) {
+      if (returnString !== '') {
+        returnString += '-';
+      }
+      returnString += string.slice(startStringLength, string.length);
+    }
+  });
+  return returnString;
+}
